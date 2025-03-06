e168ded3d23f0b54468007af580c323f0f88fbeb
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-06 16:51:23 -0300

    Mudanças para impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// using colorful terminal: Git Bash
+		// now you must open Git Bash in project's bin
+		// had to switch from JDK 11 to JDK 22
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 	}
